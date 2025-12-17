c7517a50bc5c8bf293e306a97f0f25cd4cae3337
Iqbal Habibi <habibikece100@gmail.com>
2025-12-17 10:13:30 +0700

    update 1

---
+++ Uapp/src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JFrame frame = new JFrame("Login");
+        frame.setSize(400,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        frame.setVisible(true);
+
+    }
+
+}
