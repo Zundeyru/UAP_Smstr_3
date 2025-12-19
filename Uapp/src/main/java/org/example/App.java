80f009c122e8517c5d8ef5e00d4c7820599f9e9c
setia <setiawanbimo2006@gmail.com>
2025-12-19 21:02:22 +0700

    update 4

--- Uapp/src/main/java/org/example/App.java
+++ Uapp/src/main/java/org/example/App.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class App 
@@ -13,6 +14,28 @@ public class App
         Dashboard.setLayout(new FlowLayout());
 
         JPanel buttonpanel = new JPanel(new FlowLayout());
+        buttonpanel.setBorder(new GridLayout(4,1,10,10));
+        buttonpanel.setBorder(new EmptyBorder(20,20,20,20));
+
+        JButton btnsearch = new JButton("Search Buku", btnsearcing.getIcon());
+        JButton btnhistory = new JButton("History Buku", btnsearcing.getIcon());
+
+        panel.add(label);
+        panel.add(Buku);
+        panel.add(History);
+
+        add(panel);
+        setvisible(true);
+
+        btnkembali.addActionListener(e -> {
+                    JOptionPane.showMessageDialog(this,
+                            "fitur Utama Buku library)");
+                });
+
+        btnhistory.addActionListener(e -> {
+            JOptionPane.showMessageDialog(this,
+                    "Daftar History Penambahan Buku)");
+        });
 
         JTextField Teks = new JTextField(10);
 
